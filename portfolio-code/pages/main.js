0a6c9cae4212894bb1a6ae605d0d1e7199b20bde
Stricks51 <xfurson@gmail.com>
2024-06-08 13:10:58 +0200

    Added Navbar and Scrolling Progression Bar
    It does not work yet but they have been added, saving this to have a backup in case it breaks

---
+++ portfolio-code/pages/main.js
@@ -0,0 +1,9 @@
+// scrolling progression bar for when user is scrolling
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
